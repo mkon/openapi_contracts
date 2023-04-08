4c564e934e50df2df5cc269517b6551b6ea36972
Konstantin Munteanu <konstantin@munteanu.de>
2023-04-08 10:18:32 +0200

    Fix use of rspec file

--- lib/openapi_contracts.rb
+++ lib/openapi_contracts.rb
@@ -8,9 +8,10 @@ require 'json_schemer'
 require 'yaml'
 
 module OpenapiContracts
-  autoload :Helper,   'openapi_contracts/helper'
-  autoload :Doc,      'openapi_contracts/doc'
-  autoload :Matchers, 'openapi_contracts/matchers'
+  autoload :Helper,    'openapi_contracts/helper'
+  autoload :Doc,       'openapi_contracts/doc'
+  autoload :Matchers,  'openapi_contracts/matchers'
+  autoload :Validator, 'openapi_contracts/validator'
 
   # Defines order of matching
   MATCHERS = [
@@ -23,47 +24,4 @@ module OpenapiContracts
   Env = Struct.new(:spec, :response, :expected_status)
 end
 
-if defined?(RSpec)
-  RSpec::Matchers.define :match_openapi_doc do |doc, options = {}| # rubocop:disable Metrics/BlockLength
-    include OpenapiContracts::Helper
-
-    match do |response|
-      spec = lookup_api_spec(doc, options, response)
-      env = OpenapiContracts::Env.new(spec, response, @status)
-      stack = OpenapiContracts::MATCHERS
-              .reverse
-              .reduce(->(err) { err }) { |s, m| m.new(s, env) }
-      @errors = stack.call
-      @errors.empty?
-    end
-
-    description do
-      desc = 'to match the openapi schema'
-      desc << " with #{http_status_desc(@status)}" if @status
-      desc
-    end
-
-    failure_message do |_reponse|
-      @errors.map { |e| "* #{e}" }.join("\n")
-    end
-
-    def with_http_status(status)
-      if status.is_a? Symbol
-        @status = Rack::Utils::SYMBOL_TO_STATUS_CODE[status]
-      else
-        @status = status
-      end
-      self
-    end
-
-    private
-
-    def lookup_api_spec(doc, options, response)
-      doc.response_for(
-        options.fetch(:path, response.request.path),
-        response.request.request_method.downcase,
-        response.status.to_s
-      )
-    end
-  end
-end
+require 'openapi_contracts/rspec' if defined?(RSpec)
