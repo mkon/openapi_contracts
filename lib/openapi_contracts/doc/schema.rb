8f7a56c471efa302ef21662c73bd7792e9f0b470
Konstantin Munteanu <konstantin@munteanu.de>
2023-04-19 14:02:18 +0200

    Add comments

--- lib/openapi_contracts/doc/schema.rb
+++ lib/openapi_contracts/doc/schema.rb
@@ -1,4 +1,8 @@
 module OpenapiContracts
+  # Represents a part or the whole schema
+  # Generally even parts of the schema contain the whole schema, but store the pointer to
+  # their position in the overall schema. This allows even small sub-schemas to resolve
+  # links to any other part of the schema
   class Doc::Schema
     attr_reader :path, :schema
 
@@ -7,6 +11,8 @@ module OpenapiContracts
       @path = path.freeze
     end
 
+    # Resolves Schema ref pointers links like "$ref: #/some/path" and returns new sub-schema
+    # at the target if the current schema is only a ref link.
     def follow_refs
       if (ref = dig('$ref'))
         at_path(ref.split('/')[1..])
@@ -15,6 +21,7 @@ module OpenapiContracts
       end
     end
 
+    # Generates a fragment pointer for the current schema path
     def fragment
       path.map { |p| p.gsub('/', '~1') }.join('/').then { |s| "#/#{s}" }
     end
