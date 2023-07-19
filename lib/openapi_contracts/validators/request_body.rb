718c47cb04f07d87b00d3a4f3acc75a53bccf345
Konstantin Munteanu <konstantin.munteanu@kaiahealth.com>
2023-07-19 14:41:28 +0200

    Fail on non-json payloads

--- lib/openapi_contracts/validators/request_body.rb
+++ lib/openapi_contracts/validators/request_body.rb
@@ -4,25 +4,29 @@ module OpenapiContracts::Validators
 
     private
 
+    delegate :media_type, to: :request
     delegate :request_body, to: :operation
 
-    def json_for_validation
+    def data_for_validation
+      # Support "*/json" or "*/*+json"
+      raise ArgumentError, "#{media_type.inspect} is not supported yet" unless %r{/|\+json$} =~ media_type
+
       request.body.rewind
       JSON(request.body.read)
     end
 
     def schema_for_validation
-      schema = request_body.schema_for(request.media_type)
+      schema = request_body.schema_for(media_type)
       schema.raw.merge('$ref' => schema.fragment)
     end
 
     def validate
       if !request_body
         @errors << "Undocumented request body for #{response_desc.inspect}"
-      elsif !request_body.supports_media_type?(request.media_type)
-        @errors << "Undocumented request with media-type #{request.media_type.inspect}"
+      elsif !request_body.supports_media_type?(media_type)
+        @errors << "Undocumented request with media-type #{media_type.inspect}"
       else
-        @errors += validate_schema(schema_for_validation, json_for_validation)
+        @errors += validate_schema(schema_for_validation, data_for_validation)
       end
     end
   end
--- lib/openapi_contracts/validators/response_body.rb
+++ lib/openapi_contracts/validators/response_body.rb
@@ -4,13 +4,18 @@ module OpenapiContracts::Validators
 
     private
 
-    def json_for_validation
+    delegate :media_type, to: :response
+
+    def data_for_validation
+      # Support "*/json" or "*/*+json"
+      raise ArgumentError, "#{media_type.inspect} is not supported yet" unless %r{/|\+json$} =~ media_type
+
       # ActionDispatch::Response body is a plain string, while Rack::Response returns an array
       JSON(Array.wrap(response.body).join)
     end
 
     def schema_for_validation
-      schema = spec.schema_for(response.media_type)
+      schema = spec.schema_for(media_type)
       schema.raw.merge('$ref' => schema.fragment)
     end
 
@@ -21,10 +26,10 @@ module OpenapiContracts::Validators
     def validate
       if spec.no_content?
         @errors << 'Expected empty response body' if response.body.present?
-      elsif !spec.supports_media_type?(response.media_type)
-        @errors << "Undocumented response with content-type #{response.media_type.inspect}"
+      elsif !spec.supports_media_type?(media_type)
+        @errors << "Undocumented response with content-type #{media_type.inspect}"
       else
-        @errors += validate_schema(schema_for_validation, json_for_validation)
+        @errors += validate_schema(schema_for_validation, data_for_validation)
       end
     end
   end
