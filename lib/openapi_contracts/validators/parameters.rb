b2995c073446affc2b9ced15d62ee4a305afbc57
Alfonso Jiménez <me@alfie.cat>
2025-01-10 12:47:12 +0100

    Support Ruby 3.4

--- lib/openapi_contracts/validators/parameters.rb
+++ lib/openapi_contracts/validators/parameters.rb
@@ -10,7 +10,8 @@ module OpenapiContracts::Validators
         if request.GET.key?(parameter.name)
           value = request.GET[parameter.name]
           unless parameter.matches?(value)
-            @errors << "#{value.inspect} is not a valid value for the query parameter #{parameter.name.inspect}"
+            parameter_name = parameter.name.inspect
+            @errors << "#{value.inspect.delete(' ')} is not a valid value for the query parameter #{parameter_name}"
           end
         elsif parameter.required?
           @errors << "Missing query parameter #{parameter.name.inspect}"
